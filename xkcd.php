10961cddf85e1455b8b9a1b339fe1a06ca496580
Christopher Sebastian Lara Cariceo <christopher.lara.c@gmail.com>
2020-07-04 19:40:29 -0400

    Usa funcion json_decode de PHP para generar un arreglo y obtener la url de la imagen

--- xkcd.php
+++ xkcd.php
@@ -1,3 +1,8 @@
 <?php
 
 $json = file_get_contents('https://xkcd.com/info.0.json');
+
+// El segundo parametro TRUE es para decodificar la informacion como un arreglo y no como un objeto.
+$data = json_decode($json, true); 
+
+echo $data['img'].PHP_EOL;
